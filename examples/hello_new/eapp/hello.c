d2e700537cd124d06e7ad876a887a0ba1fdd9007
Deepak Sirone Jegan <dsirone@cs.wisc.edu>
2022-01-17 17:12:25 +0530

    Fix order of dependencies

--- examples/hello_new/eapp/hello.c
+++ examples/hello_new/eapp/hello.c
@@ -7,44 +7,51 @@
 
 int main()
 {
-  printf("[NEW] hello, world!\n");
-/*
-  int sockfd;
-WOLFSSL_CTX* ctx;
-WOLFSSL* ssl;
-WOLFSSL_METHOD* method;
-struct  sockaddr_in servAddr;
-const char message[] = "Hello, World!";
+	printf("[NEW] hello, world!\n");
 
-sockfd = socket(AF_INET, SOCK_STREAM, 0);
-memset(&servAddr, 0, sizeof(servAddr));
-servAddr.sin_family = AF_INET;
-servAddr.sin_port = htons(SERV_PORT);
+	int sockfd;
+	WOLFSSL_CTX* ctx;
+	WOLFSSL* ssl;
+	WOLFSSL_METHOD* method;
+	struct  sockaddr_in servAddr;
+	const char message[] = "Hello, World!";
 
-connect(sockfd, (struct sockaddr *) &servAddr, sizeof(servAddr));
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		printf("Error creating socket\n");
+		exit(1);
+	}
 
-wolfSSL_Init();
-method = wolfTLSv1_2_client_method(); 
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_port = htons(SERV_PORT);
 
-if ( (ctx = wolfSSL_CTX_new(method)) == NULL) {
-      err_sys("wolfSSL_CTX_new error");
-}
+	if (connect(sockfd, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0 ) {
+		printf("Error connecting socket\n");
+		exit(1);
+	}
 
-if ( (ssl = wolfSSL_new(ctx)) == NULL) {
-     err_sys("wolfSSL_new error");
-}
+	wolfSSL_Init();
+	method = wolfTLSv1_2_client_method(); 
 
-if (wolfSSL_CTX_load_verify_locations(ctx, "certs/ca-cert.pem", 0) !=
-SSL_SUCCESS) {
-     err_sys("Error loading certs/ca-cert.pem");
-}
+	if ( (ctx = wolfSSL_CTX_new(method)) == NULL) {
+		err_sys("wolfSSL_CTX_new error");
+	}
 
-wolfSSL_set_fd(ssl, sockfd);
-wolfSSL_connect(ssl);
-wolfSSL_write(ssl, message, strlen(message));
+	if ( (ssl = wolfSSL_new(ctx)) == NULL) {
+     		err_sys("wolfSSL_new error");
+	}
 
-wolfSSL_free(ssl);
-wolfSSL_CTX_free(ctx);
-wolfSSL_Cleanup();*/
-  return 0;
+	if (wolfSSL_CTX_load_verify_locations(ctx, "certs/ca-cert.pem", 0) != SSL_SUCCESS) {
+     		err_sys("Error loading certs/ca-cert.pem");
+	}
+
+	wolfSSL_set_fd(ssl, sockfd);
+	wolfSSL_connect(ssl);
+	wolfSSL_write(ssl, message, strlen(message));
+
+	wolfSSL_free(ssl);
+	wolfSSL_CTX_free(ctx);
+	wolfSSL_Cleanup();
+	return 0;
 }
