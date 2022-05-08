6d8722e2ccec3bc7027689aaf34ad48a7be6df4a
Deepak Sirone Jegan <dsirone@cs.wisc.edu>
2022-05-08 18:52:23 -0500

    Working attested certificate with evidence/report

--- examples/keystore/eapp/keystore_cert.c
+++ examples/keystore/eapp/keystore_cert.c
@@ -1,7 +1,8 @@
 #include <wolfssl/options.h>
 
-#include <wolfssl/ssl.h>
-#include <wolfssl/wolfcrypt/ecc.h>
+//#include <wolfssl/ssl.h>
+//#include <wolfssl/wolfcrypt/ecc.h>
+#include <wolfssl/wolfcrypt/asn_public.h>
 #include <wolfssl/wolfcrypt/rsa.h>
 #include <wolfssl/wolfcrypt/signature.h>
 #include <wolfssl/wolfcrypt/hash.h>
@@ -11,9 +12,29 @@
 #include <sys/types.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "app/syscall.h"
+
 
 //static const unsigned char oid_keystone_evidence[] = { 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x15, 0x0A, 0x01, 0x02 };
+static byte* generateEvidenceExtension(RsaKey *gen_key, size_t *evidence_sz) {
+    byte *report_buf = (byte *)calloc(2048, sizeof(byte));
+    byte *pubkey = (byte *)calloc(1024, sizeof(byte));
+    int ret = wc_RsaKeyToPublicDer(gen_key, report_buf, 1024 * sizeof(byte));
+    if (ret < 0) {
+        printf("Error in writing public key to DER format: %d", ret);
+        return NULL;
+    }
 
+    if ((ret = attest_enclave((void *)report_buf, pubkey, ret)) != 0) {
+        printf("Error getting report %d", ret);
+        return NULL;
+    }
+
+    *evidence_sz = 2048 * sizeof(byte);
+    return report_buf;
+}
+
+//TODO: optimize buffers
 int generate_attested_cert_with_evidence(
     const unsigned char* subject_name,
     const void* optional_parameters,
@@ -21,42 +42,65 @@ int generate_attested_cert_with_evidence(
     uint8_t** output_certificate,
     size_t* output_certificate_size) {
 
-    RsaKey genKey;
-    Cert   cert;
+    RsaKey gen_key;
+    Cert   *cert = (Cert *) malloc(sizeof(Cert));
     RNG    rng;
     int    ret;
 
     printf("Before InitRng\n");
-    int r = wc_InitRng(&rng);
-    printf("InitRng returned: %d\n", r);
+    ret = wc_InitRng(&rng);
+    printf("InitRng returned: %d\n", ret);
     
     printf("Before InitRsaKey\n");
-    wc_InitRsaKey(&genKey, 0);
-    printf("Before InitCert\n");
-    wc_InitCert(&cert);
+    wc_InitRsaKey(&gen_key, 0);
+    //printf("Before InitCert\n");
+    ret = wc_InitCert(cert);
+    printf("InitCert returned: %d\n", ret);
+    printf("Sizeof Cert: %lu\n", sizeof(Cert));
 
     printf("Before MakeRSAKey\n");
-    ret = wc_MakeRsaKey(&genKey, 1024, 65537, &rng);
+    ret = wc_MakeRsaKey(&gen_key, 1024, 65537, &rng);
     
-    byte derCert[4096];
+    byte *derCert = (byte *) malloc(8000 * sizeof(byte));
 
-    strncpy(cert.subject.country, "US", CTC_NAME_SIZE);
-    strncpy(cert.subject.state, "WI", CTC_NAME_SIZE);
-    strncpy(cert.subject.locality, "Madison", CTC_NAME_SIZE);
-    strncpy(cert.subject.org, "KeystoneTAP", CTC_NAME_SIZE);
-    strncpy(cert.subject.unit, "Development", CTC_NAME_SIZE);
-    strncpy(cert.subject.commonName, "Keystore.Tap", CTC_NAME_SIZE);
-    strncpy(cert.subject.email, "info@keystoreTap", CTC_NAME_SIZE);
+    size_t evidence_sz;
+    byte *evidence_ext = generateEvidenceExtension(&gen_key, &evidence_sz);
 
+    // Initialize Certificate
+
+    strncpy(cert->subject.country, "US", CTC_NAME_SIZE);
+    strncpy(cert->subject.state, "WI", CTC_NAME_SIZE);
+    strncpy(cert->subject.locality, "Madison", CTC_NAME_SIZE);
+    strncpy(cert->subject.org, "KeystoneTAP", CTC_NAME_SIZE);
+    strncpy(cert->subject.unit, "Development", CTC_NAME_SIZE);
+    strncpy(cert->subject.commonName, "Keystore.Tap", CTC_NAME_SIZE);
+    strncpy(cert->subject.email, "info@keystoreTap", CTC_NAME_SIZE);
+
+
+    strncpy(cert->issuer.country, "US", CTC_NAME_SIZE);
+    strncpy(cert->issuer.state, "WI", CTC_NAME_SIZE);
+    strncpy(cert->issuer.locality, "Madison", CTC_NAME_SIZE);
+    strncpy(cert->issuer.org, "KeystoneTAP", CTC_NAME_SIZE);
+    strncpy(cert->issuer.unit, "Development", CTC_NAME_SIZE);
+    strncpy(cert->issuer.commonName, "Keystore.Tap", CTC_NAME_SIZE);
+    strncpy(cert->issuer.email, "info@keystoreTap", CTC_NAME_SIZE);
+
+    if ((ret = wc_SetCustomExtension(cert, 1, "1.2.3.4.5", evidence_ext, evidence_sz)) < 0) {
+        printf("Error setting evidence extension: %d", ret);
+        return ret;
+    }
+    
     printf("Before MakeSelfCert\n");
-    int certSz = wc_MakeSelfCert(&cert, derCert, sizeof(derCert), &genKey, &rng);
+    
+    int certSz = wc_MakeSelfCert(cert, derCert, 8000 * sizeof(byte), &gen_key, &rng);
     if (certSz < 0) {
         printf("Error in cert generation!\n");
     }
 
-    byte pemCert[8000];
+    
+    byte *pemCert = (byte *) malloc(8000 * sizeof(byte));
     printf("Before DerToPem\n");
-    int pemSz = wc_DerToPem(derCert, certSz, pemCert, sizeof(pemCert), CERT_TYPE);
+    int pemSz = wc_DerToPem(derCert, certSz, pemCert, 8000 * sizeof(byte), CERT_TYPE);
     if (pemSz < 0) {
         printf("Error in DER to PEM conversion\n");
     }
--- examples/keystore/libs.cmake
+++ examples/keystore/libs.cmake
@@ -1,5 +1,5 @@
 add_wolfssl(${eapp_bin}-wolfssl
-   "03deeea62b67e5543c3f29f2dd56f23e440d0f2f"
+   "96822135add15b1478b76354bb3d05ce56b83a3f"
    "musl"
-   "-DCUSTOM_RAND_GENERATE=rand_gen_keystone -DCUSTOM_RAND_TYPE=uintptr_t -DKEYSTONE"
+   "-DCUSTOM_RAND_GENERATE=rand_gen_keystone -DCUSTOM_RAND_TYPE=uintptr_t -DWOLFSSL_SMALL_STACK -DKEYSTONE -DWOLFSSL_CUSTOM_OID -DHAVE_OID_ENCODING"
 )
